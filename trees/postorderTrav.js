025da5428d1083177413a7d0cd7947adfa668e20
Zhenhua <zhenhua1109@yahoo.com>
2022-11-30 12:31:36 -0800

    add postorderTrav

---
+++ trees/postorderTrav.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Given the root of a binary tree, return the postorder traversal of
+// its nodes' values.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var postorderTraversal = function(root) {
+  let output = [];
+
+  function traverse(currNode) {
+    if (!currNode) return;
+
+    traverse(currNode.left);
+    traverse(currNode.right);
+    output.push(currNode.val);
+  }
+
+  traverse(root);
+
+  return output;
+};
